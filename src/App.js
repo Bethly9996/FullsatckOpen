38a4a3bb9294ce1d65c23a745017d96cdacc3f54
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-02-09 11:33:00 +0300

    adding state to child components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
 
+const Display = (props) => {
+  return (
+    <>
+    {props.counter}
+    </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+     { props.text}
+    </button>
+  )
+}  
+
 const App = () => {
   const [counter, setCounter] = useState(0);
   const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter -1);
   const setToZero = () => setCounter(0);
   
 
   return (
-    <>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>Zero</button>
-    </>
+    <div>
+      <Display counter={counter} /><br/>
+     <Button  onClick={increaseByOne} text='plus'/>
+     <Button onClick={decreaseByOne} text='minus'/>
+     <Button onClick={setToZero} text='zero'/>
+
+    </div>
   );
 };
 
