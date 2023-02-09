aecbd61fd1c04c95db9c0439e24adb9c9c37e5a0
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-02-09 10:54:22 +0300

    using state to rerender component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-const Hello = ({ name, age }) => {
-
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <>
-      <p>
-        Hello {name} you are {age} years old
-      </p>
-      <p>So you are probably born in {bornYear()}</p>
-    </>
-  );
-};
+import React, { useState } from "react";
 
 const App = () => {
-  const name = "Esther";
-  const age = 20;
+  const [counter, setCounter] = useState(0);
 
-  return (
-    <>
-      <h1> Greetings</h1>
-      <Hello name="Beth" age={23 + 10} />
-      <Hello name={name} age={age} />
-    </>
-  );
+  setTimeout(() => setCounter(counter + 1), 1000);
+  console.log("rendering...", counter);
+  return <div>{counter}</div>;
 };
 
 export default App;
