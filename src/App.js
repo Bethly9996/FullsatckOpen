3189a24dfa121de0f22237cd94b314c38340d900
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-02-09 11:48:59 +0300

    destructured Display nad Button components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import React, { useState } from "react";
 
-const Display = (props) => {
-  return (
-    <>
-    {props.counter}
-    </>
-  )
-}
+const Display = (counter) => {
+  return <>{counter}</>;
+};
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-     { props.text}
-    </button>
-  )
-}  
+const Button = (onClick, text) => <button onClick={onclick}>{text}</button>;
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
   const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter -1);
+  console.log("increasing, value before", counter);
+  const decreaseByOne = () => setCounter(counter - 1);
+  console.log("decreasing, value before", counter);
+  console.log("resetting to zero, value before", counter);
   const setToZero = () => setCounter(0);
-  
 
   return (
     <div>
-      <Display counter={counter} /><br/>
-     <Button  onClick={increaseByOne} text='plus'/>
-     <Button onClick={decreaseByOne} text='minus'/>
-     <Button onClick={setToZero} text='zero'/>
-
+      <Display counter={counter} />
+      <br />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
     </div>
   );
 };
