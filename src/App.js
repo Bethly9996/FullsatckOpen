ef32fce93f94d9c660e3cd1b0d511f4a3bb74f5b
Bethly9996 <bethlyc@kabarak.ac.ke>
2023-02-12 11:55:23 +0300

    worked on collecting customer feedback

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
 import React, { useState } from "react";
 
-const History = ({allClicks}) => {
-  
-  if(allClicks === 0) {
-
-    return (
-      <div>
-        the app is used by pressing the button
-      </div>
-    )
-  }
-
+const Statistics = (props) => {
   return (
     <div>
-      button press history: {allClicks.join(' ')}
+      {props.text} {props.good} {props.neutral} {props.bad} 
     </div>
-  )
-}
-
-
+  );
+};
 
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
 
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
 
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+  const isGood = () => {
+    console.log("value of good increased by one");
+    setGood(good + 1);
   };
+  const isBad = () => {
+    console.log("value of bad increased by one");
+    setBad(bad + 1);
+  };
+  const isNeutral = () => {
+    console.log("value of neutral increased by one");
+    setNeutral(neutral + 1);
+  };
+//   const isAll = () => {
+//     console.log("Total feedback");
+//     setAll(good + bad + setNeutral)
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+//   }
 
   return (
     <div>
-    {left}
-    <button onClick={handleLeftClick}>left</button>
-    <button onClick={handleRightClick}>right</button>
-    {right}
-    <History allClicks={allClicks}/>
-  </div>
+      <h1>Give Feedback</h1>
+      {good}
+      <button onClick={isGood}>Good</button>
+      {neutral}
+      <button onClick={isNeutral}>Neutral</button>
+      {bad}
+      <button onClick={isBad}>Bad</button>
+      <h2>Statistics</h2>
+      <Statistics text="Good" good={good} />
+      <Statistics  text="Neutral" neutral={neutral}/>
+      <Statistics text="Bad" bad={bad}/>
+      <Statistics text="All"/>
+      
+    </div>
   );
 };
 
